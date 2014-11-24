737760c48fdc91e66ab9e9b175987e88a587f654
Brandur <brandur@mutelight.org>
2014-11-24 00:04:01 -0800

    Revert "Revert "Add credentials for debugging""
    This reverts commit 3a62eafe6c3dac8039a1f655ee10d52e8afcdd43.

--- main.go
+++ main.go
@@ -60,6 +60,7 @@ func basicAuthPassword(r *http.Request) string {
 	}
 
 	credentials := string(buffer)
+	fmt.Printf("credentials %s\n", credentials)
 	i = strings.IndexRune(credentials, ':')
 	if i < 0 {
 		return ""
