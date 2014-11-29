be455de96fff39df0df307342dcc513a44141dc9
Brandur <brandur@mutelight.org>
2014-11-28 23:07:32 -0800

    More tests

--- receiver_test.go
+++ receiver_test.go
@@ -117,7 +117,7 @@ func TestMessageCompression(t *testing.T) {
 	actual := string(b)
 	expected := line + "\n"
 	if expected != actual {
-		t.Errorf("Expected buffer '%v', got '%v'\n", line, actual)
+		t.Errorf("Expected buffer '%v', got '%v'\n", expected, actual)
 	}
 
 	ttl, err := redis.Int(conn.Do("TTL", keyCompressed))
@@ -132,6 +132,53 @@ func TestMessageCompression(t *testing.T) {
 
 func TestMessageCompressionIncrement(t *testing.T) {
 	setup(t)
+
+	conn := connPool.Get()
+	defer conn.Close()
+
+	var writeBuffer bytes.Buffer
+	writer := gzip.NewWriter(&writeBuffer)
+	defer writer.Close()
+
+	line := "request_id=req1"
+
+	writer.Write([]byte(line + " line=1"))
+	writer.Write([]byte("\n"))
+
+	keyCompressed := Prefix + "-request_id-req1-" + CompressSuffix
+
+	writer.Close()
+	_, err := redis.Bytes(conn.Do("SET", keyCompressed, &writeBuffer))
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = subject.pushAndTrim(conf, "req1", []byte(line+" line=2"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	compressed, err := redis.Bytes(conn.Do("GET", keyCompressed))
+	if err != nil {
+		t.Error(err)
+	}
+
+	reader, err := gzip.NewReader(bytes.NewBuffer(compressed))
+	if err != nil {
+		t.Error(err)
+	}
+	defer reader.Close()
+
+	b, err := ioutil.ReadAll(reader)
+	if err != nil {
+		t.Error(err)
+	}
+
+	actual := string(b)
+	expected := line + " line=1\n" + line + " line=2\n"
+	if expected != actual {
+		t.Errorf("Expected buffer '%v', got '%v'\n", expected, actual)
+	}
 }
 
 func redisList(t *testing.T, conn redis.Conn, key string) []string {
